8abc791f428505037f6a04e7c736a1a9cef1b8cc
arad4228 <arad4228@gmail.com>
2022-10-01 15:13:56 +0900

    [NoSpear][KyoungJae] Add URL Extractor

--- tmp/fileParsing.cpp
+++ tmp/fileParsing.cpp
@@ -1,8 +1,20 @@
-#include "fileParsing.h"
+#include "ooxml.h"
+
+std::string parsing(std::string input)
+{
+    std::istringstream iss(input);
+    std::string buffer;
+    // vector를 통해 넣을 수 있지만 넣고 빼는 과정에 오버해드 생각해 해당 과정으로 처리
+    getline(iss, buffer, '"');
+    getline(iss, buffer, '"');
+    // std::cout << buffer << std::endl;
+    return buffer;
+}
 
 int main(int argc, char** argv)
 {
     void *buf = NULL;
+    std::vector<std::string> urllist;
     size_t bufsize;
 
     // Open file and load "xml" content to the document variable
@@ -22,11 +34,18 @@ int main(int argc, char** argv)
     zip_entry_close(zip);
     zip_close(zip);
 
+    // 정규표현식 정의
     std::regex re(R"(Target[\s]*=[\s]*"[a-zA-Z0-9-_.~!*'();:@&=+$,/?%#\[\]]*"[\s]*TargetMode[\s]*=[\s]*"External")");
+    // 매칭된 정규표현식 
     std::smatch match;
     while (std::regex_search(xml, match, re)) {
-        std::cout << match.str() << '\n';
+        // std::cout << match.str() << '\n';
+        urllist.push_back(parsing(match.str()));
         xml = match.suffix();
     }
+    for(int i = 0; i < urllist.size(); i++)
+        std::cout << urllist[i] << std::endl;
+
     free(buf);
+    return 0;
 }
--- tmp/fileParsing.h
+++ tmp/fileParsing.h
@@ -1,6 +1,9 @@
 #include <iostream>
 // 정규표현식을 위한 라이브러리
 #include <regex>
+#include <string>
+#include <sstream>
+
 #include <zip.h>
 // export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/arad4228/Desktop/zip/build
 // 다이나믹 라이브러리 추가
