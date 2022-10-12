9c42013d2ff939baa44fe3bce3b90793ee0e5332
arad4228 <arad4228@gmail.com>
2022-10-12 15:30:26 +0900

    [NoSpear][KyoungJae]Fix ST_BEHAVIOR name

--- src/CScriptAnalyzeEngine.cpp
+++ src/CScriptAnalyzeEngine.cpp
@@ -28,7 +28,7 @@ bool CScriptAnalyzeEngine::checkFollina(std::string script, std::vector<ST_BEHAV
     if(script.find("ms-msdt:") != std::string::npos)
     {
         ST_BEHAVIOR msdt;
-        msdt.nSeverity=2;
+        msdt.Severity=2;
         msdt.strName="Call msdt Function";
         msdt.strDesc="msdt 명령어 호출";
         vecBehaviors.push_back(msdt);
@@ -37,7 +37,7 @@ bool CScriptAnalyzeEngine::checkFollina(std::string script, std::vector<ST_BEHAV
     if(script.find("IT_RebrowseForFile") != std::string::npos)
     {
         ST_BEHAVIOR msdt;
-        msdt.nSeverity=9;
+        msdt.Severity=9;
         msdt.strName="Call IT_RebrowseForFile";
         msdt.strDesc="원격으로 실행할 프로그램을 선택할 떄 사용하는 매개변수이다.";
         vecBehaviors.push_back(msdt);
@@ -46,7 +46,7 @@ bool CScriptAnalyzeEngine::checkFollina(std::string script, std::vector<ST_BEHAV
     if(script.find("IT_LaunchMethod") != std::string::npos)
     {
         ST_BEHAVIOR msdt;
-        msdt.nSeverity=7;
+        msdt.Severity=7;
         msdt.strName="Call IT_LaunchMethod";
         msdt.strDesc="프로그램 선택과 관련이 있으며, 자동으로 선택할 프로그램 없음을 설정";
         vecBehaviors.push_back(msdt);
@@ -55,7 +55,7 @@ bool CScriptAnalyzeEngine::checkFollina(std::string script, std::vector<ST_BEHAV
     if(script.find("IT_SelectProgram") != std::string::npos)
     {
         ST_BEHAVIOR msdt;
-        msdt.nSeverity=7;
+        msdt.Severity=7;
         msdt.strName="Call IT_SelectProgram";
         msdt.strDesc="사전에 실행할 프로그램을 선택하여 프로그램 선택창을 우회할 수 있게 한다.";
         vecBehaviors.push_back(msdt);
@@ -64,7 +64,7 @@ bool CScriptAnalyzeEngine::checkFollina(std::string script, std::vector<ST_BEHAV
     if(script.find("IT_BrowseForFile") != std::string::npos)
     {
         ST_BEHAVIOR msdt;
-        msdt.nSeverity=9;
+        msdt.Severity=9;
         msdt.strName="Call IT_BrowseForFile";
         msdt.strDesc="실행할 프로그램을 선택할 떄 사용하는 매계변수이다.";
         vecBehaviors.push_back(msdt);
@@ -73,7 +73,7 @@ bool CScriptAnalyzeEngine::checkFollina(std::string script, std::vector<ST_BEHAV
     if(script.find("IT_AutoTroubleshoot") != std::string::npos)
     {
         ST_BEHAVIOR msdt;
-        msdt.nSeverity=9;
+        msdt.Severity=9;
         msdt.strName="Call IT_AutoTroubleshoot";
         msdt.strDesc="취약점 유발과 무관하나, 자동으로 문제해결 기능을 우회할 수 있게 한다.";
         vecBehaviors.push_back(msdt);
