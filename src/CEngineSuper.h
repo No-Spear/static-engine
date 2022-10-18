c222519474fb8f6adaa90b7aad0d1d51a3f5da60
arad4228 <arad4228@gmail.com>
2022-10-18 21:44:32 +0900

    [NoSpear][KyoungJae]Fix Enum location

--- src/CEngineSuper.h
+++ src/CEngineSuper.h
@@ -16,20 +16,32 @@ typedef struct ST_BEHAVIOR{
  * 정적엔진에 입력되는 입력값들에 대한 Parameter 객체
 */
 typedef struct ST_ANALYZE_PARAM{
-    std::vector<std::string> vecInputFiles;     // 검사할 문서
-    std::vector<std::string> vecURLs;           // C&C URL 리스트
-    std::vector<std::string> vecScriptFIles;    // 문서파일에서 추출한 스크립트 파일들
+    std::vector<std::pair<std::string, int> >vecInputFiles;     // 검사할 문서 또는 다운로드 파일
+    std::vector<std::string> vecURLs;                           // C&C URL 리스트
+    std::vector<std::string> vecScriptFIles;                    // 문서파일에서 추출한 스크립트 파일들
 } ST_ANALYZE_PARAM;
 
 /*
  * 정적엔진의 분석결과를 제공하기 위한 객체
 */
 typedef struct ST_ANALYZE_RESULT{
-    std::vector<std::string> vecExtractedFiles; // C&C에서 다운받은 파일들
-    std::vector<std::string> vecExtractedUrls;  // C&C URL 리스트
-    std::vector<std::string> vecExtractedScript;// 문서 파일에서 추출된 스크립트 파일
-    std::vector<ST_BEHAVIOR> vecBehaviors;      // 정적엔진 분석 결과 리스트
+    std::vector<std::pair<std::string, int> > vecExtractedFiles;// C&C에서 다운받은 파일들
+    std::vector<std::string> vecExtractedUrls;                  // C&C URL 리스트
+    std::vector<std::string> vecExtractedScript;                // 문서 파일에서 추출된 스크립트 파일
+    std::vector<ST_BEHAVIOR> vecBehaviors;                      // 정적엔진 분석 결과 리스트
 } ST_ANALYZE_RESULT;
+
+/*
+ * 엔진을 통해 다운 받은 파일에 대한 상태를 의미한다.
+*/
+enum downloadFileStatus
+{
+    CONF,   // CnC On New File
+    COOF,   // CnC On Old File   
+    CCNF,   // CnC Close New File
+    CCOF,   // CnC Close Old File
+    ASF     // Analyze Sample File
+};
 
 /*
  * 각 엔진의 부모 클래스로써
@@ -41,12 +53,6 @@ private:
     int m_nPriority;
     
 public:
-    enum{
-        CONF,
-        COOF,
-        CCNF,
-        CCOF
-    };
     CEngineSuper(int m_nPriority);
     ~CEngineSuper();
     virtual bool Analyze(const ST_ANALYZE_PARAM *input, ST_ANALYZE_RESULT *output)=0;
