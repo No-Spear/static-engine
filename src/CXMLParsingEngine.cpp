0a00a89d828d0661c2bc242326d1e792a649ea5c
kojjok97 <kojjok971@naver.com>
2022-10-26 23:33:14 +0900

    [NoSpear][hyungoo]xml파싱엔진

---
+++ src/CXMLParsingEngine.cpp
@@ -0,0 +1,115 @@
+#include "CXMLParsingEngine.h"
+
+CXMLParsingEngine::CXMLParsingEngine(){}
+
+CXMLParsingEngine::~CXMLParsingEngine(){}
+
+using std::string;
+bool CXMLParsingEngine::Analyze(const ST_ANALYZE_PARAM* input, ST_ANALYZE_RESULT* output)
+{
+
+    if(!isDocument(input->vecInputFiles[0].first))return false;
+
+    string xmlBuffer = unzipDocument(input->vecInputFiles[0].first);
+
+    if(xmlBuffer.compare("") == 0)return false;
+
+    std::cout << xmlBuffer << std::endl;
+
+    return true;
+}
+
+bool CXMLParsingEngine::isDocument(const string filePath)
+{
+    string extension = getFileExtension(filePath);
+    std::cout << extension << std::endl;
+    string fileSignature = getFileSignature(filePath);
+    std::cout << fileSignature << std::endl;
+
+    if(fileSignature.compare("807534") == 0)
+    {
+        if(extension.front() == 'd')return true;
+
+        if(extension.front() == 'x')return true;
+
+        if(extension.front() == 'p')return true;
+
+    }
+
+    std::cout << "지원하지 않는 파일 형식입니다." << std::endl;
+    return false;
+
+
+}
+
+string CXMLParsingEngine::getFileExtension(const string filePath)
+{
+
+    int location = filePath.find_last_of('.');
+
+    string extension(filePath.substr(location+1));
+
+    return extension;
+
+}
+
+string CXMLParsingEngine::getFileSignature(const string filePath)
+{
+    string fileSignature;
+    std::ifstream readFile;
+
+    readFile.open(filePath, std::ios::binary);
+
+
+    if (!readFile.is_open())
+    {
+        //에러
+    }
+
+    for(int i =0; i<4;i++)
+    {
+        int binary = readFile.get();
+        std::cout << binary << std::endl;
+        fileSignature += std::to_string(binary);
+    }
+    return fileSignature;
+}
+
+string CXMLParsingEngine::unzipDocument(const string filePath)
+{   
+    std::regex re("\\<\\?.*\\?\\>");
+    string xmlBuffer;
+    this->OOXML = zip_open(filePath.c_str(), ZIP_DEFAULT_COMPRESSION_LEVEL, 'r');
+    if(this->OOXML == NULL)
+    {
+        std::cout << "파일을 열 수 없습니다." << std::endl;
+        return xmlBuffer;
+    }
+
+    int i, n = zip_entries_total(OOXML);
+    for (i = 0; i < n; i++)
+    {  
+        zip_entry_openbyindex(OOXML,i);
+        {
+            
+            
+            int isdir = zip_entry_isdir(OOXML);
+            if(isdir == 1)continue;
+            string name(string(zip_entry_name(OOXML)));
+            if(name.find(".rels")==string::npos && name.find(".xml") == string::npos)continue;
+            char * buf = NULL;
+            size_t bufsize= 0;
+            zip_entry_read(OOXML, (void **)&buf, &bufsize);
+            string tempBuf = string(buf);        
+            if(i == 0) xmlBuffer = xmlBuffer + tempBuf; 
+            if(i == 0) continue;
+            tempBuf = std::regex_replace(tempBuf,re,"");
+
+            xmlBuffer = xmlBuffer + tempBuf;
+            
+        }  
+
+    }
+    return xmlBuffer;
+
+}
---
+++ src/CXMLParsingEngine.h
@@ -0,0 +1,27 @@
+#include "CEngineSuper.h"
+#include "CException.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <regex> 
+#include <zip.h>
+
+using std::string;
+
+class CXMLParsingEngine
+{
+public:
+    CXMLParsingEngine();
+    ~CXMLParsingEngine();
+    bool Analyze(const ST_ANALYZE_PARAM* input, ST_ANALYZE_RESULT* output);
+
+private:
+    zip_t* OOXML;
+    void* buffer;
+    size_t bufferSize;
+    bool isDocument(const string filePath);
+    string getFileExtension(const string filePath);
+    string getFileSignature(const string filePath);
+    string unzipDocument(const string filePath);
+};
