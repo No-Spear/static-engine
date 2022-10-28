c16859602d55f83192abd806a694eae159df2e21
arad4228 <arad4228@gmail.com>
2022-10-28 22:50:52 +0900

    [NoSpear][KyoungJae]Fix Analyze typo & Fix Severity Follina

--- src/CScriptAnalyzeEngine.cpp
+++ src/CScriptAnalyzeEngine.cpp
@@ -44,7 +44,7 @@ bool CScriptAnalyzeEngine::checkFollina(std::string script, std::string url ,std
     {
         ST_BEHAVIOR msdt;
         msdt.strUrl.append(url);
-        msdt.Severity=6;
+        msdt.Severity=7;
         msdt.strName="Call msdt Function";
         msdt.strDesc="msdt 명령어 호출";
         vecBehaviors.push_back(msdt);
@@ -64,7 +64,7 @@ bool CScriptAnalyzeEngine::checkFollina(std::string script, std::string url ,std
     {
         ST_BEHAVIOR msdt;
         msdt.strUrl.append(url);
-        msdt.Severity=7;
+        msdt.Severity=8;
         msdt.strName="Use IT_LaunchMethod";
         msdt.strDesc="프로그램 선택과 관련이 있으며, 자동으로 선택할 프로그램 없음을 설정";
         vecBehaviors.push_back(msdt);
@@ -74,7 +74,7 @@ bool CScriptAnalyzeEngine::checkFollina(std::string script, std::string url ,std
     {
         ST_BEHAVIOR msdt;
         msdt.strUrl.append(url);
-        msdt.Severity=7;
+        msdt.Severity=8;
         msdt.strName="Use IT_SelectProgram";
         msdt.strDesc="사전에 실행할 프로그램을 선택하여 프로그램 선택창을 우회할 수 있게 한다.";
         vecBehaviors.push_back(msdt);
@@ -86,7 +86,7 @@ bool CScriptAnalyzeEngine::checkFollina(std::string script, std::string url ,std
         msdt.strUrl.append(url);
         msdt.Severity=9;
         msdt.strName="Use IT_BrowseForFile";
-        msdt.strDesc="실행할 프로그램을 선택할 떄 사용하는 매계변수이다.";
+        msdt.strDesc="실행할 프로그램을 선택할 떄 사용하는 매개변수이다.";
         vecBehaviors.push_back(msdt);
     }
 
