4defcc356c4b0d6f61aab1f60b257f12853c9576
arad4228 <arad4228@gmail.com>
2022-12-14 13:15:49 +0900

    [NoSpear][KyoungJae]Move Base64 Decoder from ScriptAnalyzeEngine

--- src/SpearCutter/HelpFunc.cpp
+++ src/SpearCutter/HelpFunc.cpp
@@ -44,3 +44,34 @@ bool sendResultToServer(const char* pipe, const ST_REPORT outReport)
     close(filedes);
     return true;
 }
+
+// base64 디코더
+std::string base64_decoder(const std::string& input)
+{
+    static const std::string b = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+ 
+    std::string result;
+    std::vector<int> T(256, -1);
+ 
+    for (int i = 0; i < 64; i++)
+        T[b[i]] = i;
+ 
+    int val = 0;
+    int valb = -8;
+ 
+    for (u_char c : input) 
+    {
+        if (T[c] == -1)
+            break;
+ 
+        val = (val << 6) + T[c];
+        valb += 6;
+ 
+        if (valb >= 0) 
+        {
+            result.push_back(char((val >> valb) & 0xFF));
+            valb -= 8;
+        }
+    }
+    return result;
+}
--- src/SpearCutter/HelpFunc.h
+++ src/SpearCutter/HelpFunc.h
@@ -4,3 +4,5 @@
 struct ST_REPORT;
 
 bool sendResultToServer(const char* pipe, const ST_REPORT report);
+
+std::string base64_decoder(const std::string& input);
